b5b1de5a0ea73501e32036c00c82c3dfdd117029
Ladan Torkzaban <99889241+ladantork@users.noreply.github.com>
2024-04-28 22:23:59 +0100

    api-hosting

--- listen.js
+++ listen.js
@@ -1,6 +1,7 @@
 const app = require('./app.js');
-
-app.listen(9090, (err) => {
-    if (err) console.log(err);
-    else console.log('Server is listening on port 9090...');
-  });
+const { PORT = 9090 } = process.env;
+// app.listen(9090, (err) => {
+//     if (err) console.log(err);
+//     else console.log('Server is listening on port 9090...');
+//   });
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
